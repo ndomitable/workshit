a6001224e9b0765cdca248a0fe5c001083a59557
ndomitable <133073476+ndomitable@users.noreply.github.com>
2023-10-06 13:32:04 -0700

    Update and rename NewOpens.sql to NewOpens2.sql

---
+++ NewOpens2.sql
@@ -0,0 +1,7 @@
+SELECT *
+FROM newopens x
+WHERE closedate >= '2023-06-01' AND closedate < '2023-07-01'
+AND NOT EXISTS(SELECT 1 
+				FROM t2
+                WHERE `Associated Company` = x.`Associated Company` AND closedate < '2023-06-01')
+;
