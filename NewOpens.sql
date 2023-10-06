2aa7bc0067a9799b787f4f977e55dcab4af9641b
ndomitable <133073476+ndomitable@users.noreply.github.com>
2023-10-06 13:29:31 -0700

    Update NewOpens.sql

--- NewOpens.sql
+++ NewOpens.sql
@@ -1,5 +1,22 @@
+CREATE TABLE newopens
+SELECT
+	`Record ID`,
+ 	`Associated Company`, 
+    `Associated Company IDs`,
+    `Deal Name`,
+     `Deal owner`,
+     Amount, 
+     substring_index(`Close Date`, ' ', 1) as closedate,
+     ROW_NUMBER() OVER (PARTITION BY `Associated Company` ORDER BY `Close Date` DESC) as rn
+FROM sales;
+
+
+
+
+
+
 SELECT *
-FROM t2 x
+FROM newopens x
 WHERE closedate >= '2023-06-01' AND closedate < '2023-07-01'
 AND NOT EXISTS(SELECT 1 
 				FROM t2
